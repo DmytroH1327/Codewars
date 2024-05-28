03ecafee440689d9c1d931294266da5ec61aa831
Dmytro Hladchenko <gladchenko.do@gmail.com>
2024-05-28 23:10:36 +0200

    Add files via upload

---
+++ 8 kyu/Array plus array.py
@@ -0,0 +1,10 @@
+# I'm new to coding and now I want to get the sum of two arrays...
+# Actually the sum of all their elements. I'll appreciate for your help.
+
+def array_plus_array(arr1,arr2):
+    return sum(arr1+arr2)
+
+
+
+assert array_plus_array([1, 2, 3], [4, 5, 6]) == 21
+print("OK")
